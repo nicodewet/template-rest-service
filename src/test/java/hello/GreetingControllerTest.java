5c7f0f7a3485fef1b67f7b331316b40eb3c4ebb7
Nico de Wet <nico@nicodewet.com>
2015-08-21 17:00:43 +1200

    Spring Boot @IntegrationTest for REST GreetingsController using RestAssured library

---
+++ src/test/java/hello/GreetingControllerTest.java
@@ -0,0 +1,49 @@
+package hello;
+
+import static com.jayway.restassured.RestAssured.when;
+
+import org.apache.http.HttpStatus;
+import org.hamcrest.Matchers;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.test.IntegrationTest;
+import org.springframework.boot.test.SpringApplicationConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.springframework.test.context.web.WebAppConfiguration;
+
+import com.jayway.restassured.RestAssured;
+
+// http://www.jayway.com/2014/07/04/integration-testing-a-spring-boot-application/
+
+@RunWith(SpringJUnit4ClassRunner.class)   // 1
+@SpringApplicationConfiguration(classes = Application.class)   // 2
+@WebAppConfiguration   // 3
+@IntegrationTest("server.port:0")   // 4
+public class GreetingControllerTest {
+
+	@Value("${local.server.port}")   // 6
+	int port;
+
+	Greeting helloWorld;
+
+	@Before
+	public void setUp() {
+		helloWorld = new Greeting(1L, "Hello, World!");
+		// 9
+		RestAssured.port = port;
+	}
+
+	// 10
+    @Test
+    public void canFetchFirstHelloWorldGreeting() {
+        when().
+                get("/greeting").
+        then().
+                statusCode(HttpStatus.SC_OK).
+                body("id", Matchers.is(1)).
+                body("content", Matchers.is("Hello, World!"));
+    }
+
+}
